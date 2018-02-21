efdb54f8b010c7dc25e0844be8d2a0672b3a46a0
Pi0trS <piotrsy82@gmail.com>
2018-02-22 00:09:49 +0100

    Add list

--- List.h
+++ List.h
@@ -1,2 +1,11 @@
 #pragma once
-class 
+#include "Container.h"
+class List
+{
+public:
+	Conteiner *first;
+	List();
+	void add_container(Conteiner to_add);
+	void remove_container(Conteiner to_remove);
+
+};
